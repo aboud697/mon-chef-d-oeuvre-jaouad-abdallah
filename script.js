84172631e38e8319a10cdb99f746590647cba88b
Aboud <jaouadabdallah76@gmail.com>
2025-11-13 14:20:22 +0000

    feat: Ajout de script.js et première interaction DOM

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Le script est chargé !")
+
+const titre = document.querySelector('#titre-principal')
+if (titre) {
+  titre.addEventListener('click', () => {
+    console.log("J'ai cliqué sur le titre !");
+    titre.textContent = "Titre cliqué !";
+  });
+}
